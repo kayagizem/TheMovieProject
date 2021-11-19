06e13578ded2cfc44936887a1eac67c52c80dd0f
Gizem Kaya <gizemkaya17@ku.edu.tr>
2021-11-19 15:05:51 +0300

    Initial Commit

---
+++ TheMovieProject/CarouselItem.swift
@@ -0,0 +1,30 @@
+//
+//  CarouselItem.swift
+//  TheMovieProject
+//
+//  Created by gizem.kaya on 18.11.2021.
+//
+import Foundation
+import UIKit
+
+class CarouselItem: UIView {
+
+    @IBOutlet var vwContent: UIView!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        initWithNib()
+    
+    }
+ 
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ TheMovieProject/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieProject
+//
+//  Created by gizem.kaya on 18.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var vwContainer: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
