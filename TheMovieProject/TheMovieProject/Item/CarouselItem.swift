3597d30b8c9b56d65becb0b451bc0366ee047277
Gizem Kaya <72571801+kayagizem@users.noreply.github.com>
2021-11-19 15:18:31 +0300

    Add files via upload

---
+++ TheMovieProject/TheMovieProject/Item/CarouselItem.swift
@@ -0,0 +1,37 @@
+import Foundation
+import UIKit
+
+@IBDesignable
+class CarouselItem: UIView {
+    static let CAROUSEL_ITEM_NIB = "carouselItem"
+    
+    @IBOutlet var vwContent: UIView!
+    @IBOutlet weak var vwBackground: UIView!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initWithNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initWithNib()
+    }
+    
+    convenience init(imageName: UIImage?, background: UIColor? = .red) {
+        self.init()
+        image.image = imageName
+        vwBackground.backgroundColor = background
+    }
+    
+    fileprivate func initWithNib() {
+        Bundle.main.loadNibNamed(CarouselItem.CAROUSEL_ITEM_NIB, owner: self, options: nil)
+        vwContent.frame = bounds
+        vwContent.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(vwContent)
+    }
+}
---
+++ TheMovieProject/TheMovieProject/View Controllers/CarouselPageViewController.swift
@@ -0,0 +1,99 @@
+import Foundation
+import UIKit
+
+class CarouselPageViewController: UIPageViewController {
+    
+    fileprivate var items: [UIViewController] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        decoratePageControl()
+        populateItems()
+        
+        if let firstViewController = items.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    fileprivate func decoratePageControl() {
+        let pc = UIPageControl.appearance(whenContainedInInstancesOf: [CarouselPageViewController.self])
+        pc.currentPageIndicatorTintColor = UIColor(red: 2.0 / 255.0, green: 148.0 / 255.0, blue: 165.0 / 255.0, alpha: 0.4)
+        pc.pageIndicatorTintColor = .white
+        pc.backgroundColor = UIColor.clear
+        pc.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    
+    fileprivate func populateItems() {
+        let images: [UIImage] = [
+            UIImage(named: "background1")!,
+            UIImage(named: "background2")!,
+            UIImage(named: "background3")!
+        ]
+        let backgroundColor:[UIColor] = [.blue, .red, .green]
+        
+        for (index, t) in images.enumerated() {
+            let c = createCarouselItemControler(with: t, with: backgroundColor[index])
+            items.append(c)
+        }
+    }
+    
+    fileprivate func createCarouselItemControler(with imageTitle: UIImage?, with color: UIColor?) -> UIViewController {
+        let c = UIViewController()
+        c.view = CarouselItem(imageName: imageTitle, background: color)
+        return c
+    }
+}
+
+// MARK: - DataSource
+
+extension CarouselPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = items.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return items.last
+        }
+        
+        guard items.count > previousIndex else {
+            return nil
+        }
+        
+        return items[previousIndex]
+    }
+    
+    func pageViewController(_: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = items.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        guard items.count != nextIndex else {
+            return items.first
+        }
+        
+        guard items.count > nextIndex else {
+            return nil
+        }
+        
+        return items[nextIndex]
+    }
+    
+    func presentationCount(for _: UIPageViewController) -> Int {
+        return items.count
+    }
+    
+    func presentationIndex(for _: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+              let firstViewControllerIndex = items.firstIndex(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
